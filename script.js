7f0a921d9aff49fef684ffa08736d5b29d95d7c6
soffiayanchashvili <119891979+soffiayanchashvili@users.noreply.github.com>
2023-09-12 23:01:45 +0400

    testing

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const firsPage = document.createElement("div");
 
-firsPage.innerHTML = "hello world";
- document.body.appendChild(firsPage)
+firsPage.style.background = "rgba(37, 43, 66, 1)";
+firsPage.style.display = "flex";
+firsPage.style.alignItems = "center";
+firsPage.style.justifyContent = "center";
+
+const welcome = document.createElement("p");
+welcome.innerHTML = "Welcome";
+welcome.style.color = "primary-color";
+welcome.style.fontSize = "16px";
+
+const title = document.createElement("h1");
+title.innerText = "Selling on the internet like a pro";
+title.style.color = "white";
+title.style.fontSize = "58px";
+
+conts 
+
+document.body.appendChild(firsPage);
